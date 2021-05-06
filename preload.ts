83453b1cc668fe4bd2f3ee820471126c495a3c55
AtomJon <jonbruskin@gmail.com>
2021-05-06 18:44:38 +0200

    .

---
+++ preload.ts
@@ -0,0 +1,32 @@
+const Units = ["", ""];
+
+interface ingredient {
+  Name : string
+  Amount : number
+  Unit : any;
+}
+
+interface Recipe {
+  Name : string;
+  
+  Ingredients : Array<ingredient>;
+  Amount : number;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  
+
+  let recipeList = document.getElementById("recipe-list")
+
+  let testRecipe:Recipe =  {
+    Name:"Spaghetti med kødboller",
+    Amount:8,
+    Ingredients:[
+      {Name:"Spaghetti", Amount:200, Unit:"kg"},
+      {Name:"Kødboller", Amount:2, Unit:""},
+    ]};
+});
+
+var addRecipe : (list : HTMLElement, recipe : Recipe) => {
+  let ele = "<p></p>";
+}
